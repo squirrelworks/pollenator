83041c734ea3a19e711588ee0505aaafb1e49bb2
bo-nicolaisen <bo@squirrelworks.dk>
2024-03-20 22:49:43 +0100

    base

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,1 +1,35 @@
 // write cool JS hwere!!
+
+getLocation();
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(getPollenData);
+    console.log(myPos);
+  } else {
+    document.getElementById("demo").innerHTML = "Geolocation is not supported";
+  }
+}
+
+function getPollenData(position) {
+  var latitude = position.coords.latitude;
+  var longitude = position.coords.longitude;
+
+  let myUrl = `https://air-quality-api.open-meteo.com/v1/air-quality?latitude=${latitude}&longitude=${longitude}&current=alder_pollen,birch_pollen,grass_pollen,ragweed_pollen&hourly=alder_pollen,birch_pollen,grass_pollen,mugwort_pollen,ragweed_pollen&timezone=Europe%2FBerlin&domains=cams_europe`;
+
+  fetch(myUrl)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      // Handle the data
+      console.log(data);
+    })
+    .catch((error) => {
+      // Handle any errors that occurred during the fetch
+      console.error("Fetch error:", error);
+    });
+}
